3976fc05382e58a880a0d30c40c5d73c9f0e039a
prantosheikh <prantosheikh834@gmail.com>
2023-11-17 22:12:30 +0600

    lets start booking page codeing

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Container from "../../components/Shared/Container/Container";
 
 const Booking = () => {
-    const {id} = useParams()
-    console.log(id);
-    return (
-        <div>
-            <h2>Booking</h2>
-        </div>
-    );
+  const { id } = useParams();
+  console.log(id);
+
+  const [trips, setTrips] = useState([]);
+  console.log(trips);
+  useEffect(() => {
+    fetch(`http://localhost:3000/featured-details/${id}`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTrips(data);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <h2>Booking</h2>
+    </Container>
+  );
 };
 
 export default Booking;
