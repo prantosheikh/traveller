160a2a34b118f30b481e87e4b211f3796b3a9361
prantosheikh <prantosheikh834@gmail.com>
2023-11-18 02:46:01 +0600

    booking page processing

--- src/components/Shared/Container/Container.jsx
+++ src/components/Shared/Container/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
     return (
-      <section className="max-w-[1520px] px-5 my-20 mx-auto">{children}</section>
+      <section className="max-w-[1520px] bg-white px-5 my-20 mx-auto">{children}</section>
     );
   };
   
--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { FaRegHeart } from "react-icons/fa";
+import { CiMoneyCheck1 } from "react-icons/ci";
+import { FaRegFlag, FaRegHeart } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { MdGroups } from "react-icons/md";
 import { RiShareBoxFill } from "react-icons/ri";
+import { WiTime10 } from "react-icons/wi";
+
 import Rating from "react-rating";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Container from "../../components/Shared/Container/Container";
 
 import { CiLocationOn } from "react-icons/ci";
@@ -14,7 +19,7 @@ const Booking = () => {
   console.log(id);
 
   const [trips, setTrips] = useState([]);
-  console.log(trips?.reviews);
+  console.log(trips);
 
   useEffect(() => {
     fetch(`http://localhost:3000/featured-details/${id}`, {
@@ -29,10 +34,16 @@ const Booking = () => {
 
   return (
     <Container>
-      <div className="pt-16">
-        <h2 className="text-3xl">{trips?.title}</h2>
+      <div className="pt-6 bg-white">
+        <h1 className="flex items-center gap-4 text-base">
+          <Link to="/">Home</Link> <IoIosArrowForward /> {trips?.title}
+        </h1>
+        <button className="py-2 px-4 bg-orange-50 text-orange-600 rounded-full my-4">
+          Nature Tours{" "}
+        </button>
+        <h2 className="text-3xl font-bold">{trips?.title}</h2>
 
-        <div className="flex justify-between items-center my-4">
+        <div className=" flex justify-between items-center my-4">
           <div className="flex items-center gap-2 text-base ">
             {trips?.reviews && trips.reviews.length > 0 ? (
               <>
@@ -71,6 +82,95 @@ const Booking = () => {
               <span>Wishlist</span>
             </div>
           </div>
+        </div>
+
+        {/* Photo */}
+        <div className="flex justify-between items-center gap-6">
+          <div className="w-[60%] ">
+            <div className="relative overflow-hidden rounded-lg group">
+              <img
+                src={trips.pOne}
+                alt={`Image`}
+                className="object-cover w-full h-full scale-100 group-hover:scale-110 duration-500"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 w-[39%]">
+            <div className="relative overflow-hidden rounded-lg group">
+              <img
+                src={trips.pTwo}
+                alt={`Image`}
+                className="object-cover w-full h-full scale-100 group-hover:scale-110 duration-500"
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 items-center">
+              <div className="relative overflow-hidden rounded-lg group">
+                <img
+                  src={trips.pThree}
+                  alt={`Image`}
+                  className="object-cover w-full h-full scale-100 group-hover:scale-110 duration-500"
+                />
+              </div>
+              <div className="relative overflow-hidden rounded-lg group">
+                <img
+                  src={trips.pFour}
+                  alt={`Image`}
+                  className="object-cover w-full h-full scale-100 group-hover:scale-110 duration-500"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Another Section  */}
+
+        <div className="flex justify-between gap-4 my-10">
+          <div className="w-[70%]">
+            <div className="grid grid-cols-4 items-center">
+              <div className="flex gap-3 items-center">
+                <div className="border rounded-lg p-4">
+                  <FaRegFlag className="text-xl" />
+                </div>
+                <div>
+                  <p className="text-base  text-gray-700">Tour Code</p>
+                  <p className="text-gray-700">{trips?.tourCode}</p>
+                </div>
+              </div>
+              
+              <div className="flex gap-3 items-center">
+                <div className="border rounded-lg p-4">
+                  <WiTime10 className="text-xl" />
+                </div>
+                <div>
+                  <p className="text-base text-gray-700">Duratio</p>
+                  <p className="text-gray-700">{trips?.duration}</p>
+                </div>
+              </div>
+           
+              <div className="flex gap-3 items-center">
+                <div className="border rounded-lg p-4">
+                  <MdGroups className="text-xl" />
+                </div>
+                <div>
+                  <p className="text-base text-gray-700">Guests</p>
+                  <p className="text-gray-700">{trips?.guests}</p>
+                </div>
+              </div>
+              <div className="flex gap-3 items-center">
+                <div className="border rounded-lg p-4">
+                  <CiMoneyCheck1 className="text-xl" />
+                </div>
+                <div>
+                  <p className="text-base text-gray-700">Price</p>
+                  <p className="text-gray-700"> from {trips?.price}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%]">2</div>
+        </div>
+        <div>
+            
         </div>
       </div>
     </Container>
